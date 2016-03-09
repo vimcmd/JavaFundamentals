74eb128664fbb608411f49343f63a84c45ef5237
vim.cmd <vim.cmd@gmail.com>
2016-03-09 11:06:57 +0500

    Initial commit

---
+++ src/com/newcircle/bookshelf/javaFundamentals/chapter10_javaLangPackage/part01_primitiveWrappers/Example.java
@@ -0,0 +1,8 @@
+package com.newcircle.bookshelf.javaFundamentals.chapter10_javaLangPackage.part01_primitiveWrappers;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(Integer.class);
+        System.out.println("Integer range: " + Integer.MIN_VALUE + ".." + Integer.MAX_VALUE);
+    }
+}
