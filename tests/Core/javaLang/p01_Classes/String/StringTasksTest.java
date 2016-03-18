e2f4b36cc3d00862c95616368005a6fb21fa2504
vim.cmd <vim.cmd@gmail.com>
2016-03-18 16:11:54 +0500

    added tests for class methods

--- tests/Core/javaLang/p01_Classes/String/StringTasksTest.java
+++ tests/Core/javaLang/p01_Classes/String/StringTasksTest.java
@@ -2,21 +2,67 @@ package Core.javaLang.p01_Classes.String;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class StringTasksTest {
 
+    public static final String initialStringToReverse = "AbCd@3.";
+    public static final String reversedString = ".3@dCbA";
+    public static final String initialStringToSortAlphabetically = "   Alena   Alice     alina albina     Anastasya   ALLA ArinA     ";
+    public static final String sortedString = "albina Alena Alice alina ALLA Anastasya ArinA";
+
+
     /**
-     * Basically, it tests default JSE methods - StringBuilder.reverse()
+     * Basically, it tests default JSE API - StringBuilder.reverse()
      * @throws Exception
      */
     @Test
     public void testStringReverse() throws Exception {
-        String initialString = "AbCd@3.";
-        String reversedString = ".3@dCbA";
-
-        StringTasks stringExample = new StringTasks(initialString);
+        StringTasks stringExample = new StringTasks(initialStringToReverse);
         stringExample.stringReverse();
         assertEquals("Reversed string wrong", reversedString, stringExample.getResultString());
+    }
+
+
+    @Test
+    public void testSortWordsInString() throws Exception {
+        StringTasks stringSort = new StringTasks(initialStringToSortAlphabetically);
+        stringSort.sortWordsInStringIgnoreCase();
+        assertEquals("Sorting words in string goes wrong", sortedString, stringSort.getResultString());
+    }
+
+
+    @Test
+    public void testSortWordsInStringDefault() throws Exception {
+        StringTasks stringSort = new StringTasks(initialStringToSortAlphabetically);
+        stringSort.sortWordsInStringIgnoreCaseUsingArrays();
+        assertEquals("Sorting words in string goes wrong", sortedString, stringSort.getResultString());
+    }
+
+    @Test
+    public void testSortWordsInStringStreamDefault() throws Exception {
+        StringTasks stringSort = new StringTasks(initialStringToSortAlphabetically);
+        stringSort.sortWordsInStringIgnoreCaseUsingStream();
+        assertEquals("Sorting words in string goes wrong", sortedString, stringSort.getResultString());
+    }
+
+    @Test
+    public void testReplaceCharInEachWord() throws Exception {
+        final String initialString = "abc DECVFCNF 1234 ad   vr";
+        final String expectedString = "abX DEXVFCNF 12X4 ad   vr";
+        StringTasks string = new StringTasks(initialString);
+        final char replacingChar = 'X';
+        string.replaceCharInEachWord(2, replacingChar);
+        assertEquals(expectedString, string.getResultString());
+    }
+
+    @Test
+    public void testReplaceEachCharInEachWord() throws Exception {
+        final String initialString = "abc DECVFCNF 1234 ad   vr";
+        final String expectedString = "XXX XXXXXXXX XXXX XX   XX";
+        StringTasks string = new StringTasks(initialString);
+        final char replacingChar = 'X';
+        string.replaceCharInEachWord(1, replacingChar);
+        assertEquals(expectedString, string.getResultString());
     }
 }
