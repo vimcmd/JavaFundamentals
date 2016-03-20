b815bfa96de242a20bf061b67cfd7288bc545a06
vimcmd <vim.cmd@gmail.com>
2016-03-20 16:45:46 +0500

    added example of string intern

---
+++ src/p02_classesAndLibrariesUsing/ch07_strings/sub01_stringClass/StringIntern.java
@@ -0,0 +1,20 @@
+package p02_classesAndLibrariesUsing.ch07_strings.sub01_stringClass;
+
+/**
+ * Inclusion in literal pool.
+ */
+public class StringIntern {
+    public static void main(String[] args) {
+
+        // Reference s1 initialises with literal, having all object properties.
+        String s1 = "Java";
+        String s2 = new String("Java");
+
+        System.out.println(s1 == s2); //false
+
+        // Method intern() invoking organizes search in "literal pool" based on s2 value, and on positive search returns
+        // reference to found literal, else includes s2 to literal pool and returns reference.
+        s2 = s2.intern();
+        System.out.println(s1 == s2); //true
+    }
+}
