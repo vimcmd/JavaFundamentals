8d899b4597f3949236da9df7bbe2e98b6b110e68
vim.cmd <vim.cmd@gmail.com>
2016-03-21 11:22:28 +0500

    added example of using regular expressions

---
+++ src/p02_classesAndLibrariesUsing/ch07_strings/sub03_regularExpressions/RegularExample.java
@@ -0,0 +1,55 @@
+package p02_classesAndLibrariesUsing.ch07_strings.sub03_regularExpressions;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <p>Processing string using patterns</p>
+ * <p>{@link Pattern} class used for defining regular expressions (patterns), which is searched in string, file
+ * or other object of a sequence of characters.</p>
+ * <p>On each matching can be obtained information using class {@link Matcher}</p>
+ */
+public class RegularExample {
+    public static void main(String[] args) {
+        System.out.println(isStringMatchesPattern("a+y", "aaay"));
+        searchStringMatchingRegex("(\\w{6,})@(\\w+\\.)([a-z]{2,4})", "email addresses: pupkin@gmail.com, ivanov@bsy.by!");
+        splitStringUsingRegex("\\s?\\d+\\s?", "java5tiger 77 java6mustang");
+    }
+
+    /**
+     * Check if string matches pattern
+     *
+     * @param regex
+     * @param input
+     */
+    private static boolean isStringMatchesPattern(String regex, String input) {
+        Pattern p1 = Pattern.compile(regex);
+        Matcher m1 = p1.matcher(input);
+        return m1.matches();
+    }
+
+    /**
+     * Search and select substring matches specified pattern
+     *
+     * @param regex
+     * @param input
+     */
+    private static void searchStringMatchingRegex(String regex, String input) {
+        Pattern p2 = Pattern.compile(regex);
+        Matcher m2 = p2.matcher(input);
+
+        while (m2.find()) {
+            System.out.println(m2.group());
+        }
+    }
+
+    /**
+     * split string to substrings using pattern as delimiter
+     */
+    private static void splitStringUsingRegex(String regex, String input) {
+        Pattern p3 = Pattern.compile(regex);
+        String[] words = p3.split(input);
+        System.out.println(Arrays.toString(words));
+    }
+}
---
+++ src/p02_classesAndLibrariesUsing/ch07_strings/sub03_regularExpressions/RegularGroups.java
@@ -0,0 +1,11 @@
+package p02_classesAndLibrariesUsing.ch07_strings.sub03_regularExpressions;
+
+public class RegularGroups {
+    public static void main(String[] args) {
+
+    }
+
+    public static void simpleMatching(String regex, String input) {
+
+    }
+}
