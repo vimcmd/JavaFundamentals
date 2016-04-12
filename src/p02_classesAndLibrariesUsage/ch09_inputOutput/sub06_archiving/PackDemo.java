8733b82851b6cafd4b273beb87fc2e1838ea91af
vim.cmd <vim.cmd@gmail.com>
2016-04-12 11:05:41 +0500

    added example of archiving files in directory

---
+++ src/p02_classesAndLibrariesUsage/ch09_inputOutput/sub06_archiving/PackDemo.java
@@ -0,0 +1,18 @@
+package p02_classesAndLibrariesUsage.ch09_inputOutput.sub06_archiving;
+
+/* # 13 # start archiving process */
+
+import java.io.FileNotFoundException;
+
+public class PackDemo {
+    public static void main(String[] args) {
+        String dirName = "src\\p02_classesAndLibrariesUsage\\ch09_inputOutput\\sub06_archiving";
+        PackJar pj = new PackJar("example.jar");
+
+        try {
+            pj.pack(dirName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
