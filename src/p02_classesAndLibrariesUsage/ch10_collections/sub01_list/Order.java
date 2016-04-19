b8eadb7c8a37fa6fb333bf8b2de84d1f5df03dc3
vim.cmd <vim.cmd@gmail.com>
2016-04-19 12:38:35 +0500

    added example of unchecked (raw) and checked generic collection

---
+++ src/p02_classesAndLibrariesUsage/ch10_collections/sub01_list/Order.java
@@ -0,0 +1,29 @@
+package p02_classesAndLibrariesUsage.ch10_collections.sub01_list;
+
+/* # 3 # helper class Order*/
+
+public class Order {
+    private int orderId;
+    private float amount;
+
+    public Order(int orderId, float amount) {
+        this.orderId = orderId;
+        this.amount = amount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", amount=" + amount +
+                '}';
+    }
+}
---
+++ src/p02_classesAndLibrariesUsage/ch10_collections/sub01_list/UncheckedAndCheckedCollection.java
@@ -0,0 +1,39 @@
+package p02_classesAndLibrariesUsage.ch10_collections.sub01_list;
+
+import java.util.ArrayList;
+
+/* # 2 # Incorrect (raw objects) and correct collections */
+
+public class UncheckedAndCheckedCollection {
+    public static void main(String[] args) {
+        ArrayList raw = new ArrayList(){ // raw type collection
+            { // logic block of anonymous class with unchecked call
+                add(new Order(231, 12.f));
+                add(new Item(5465, 120.0f, "Xerox"));
+                add(new Order(321, 10.f));
+            }
+        };
+
+        // on data extraction type casting required
+        Order or1 = (Order) raw.get(0);
+        Item or2 = (Item) raw.get(1);
+        Order or3 = (Order) raw.get(2);
+
+        for(Object ob : raw) {
+            System.out.println("raw: " + ob);
+        }
+
+        ArrayList<Order> orders = new ArrayList<Order>(){
+            {
+                add(new Order(231, 12.f));
+                add(new Order(513, 11.f));
+                add(new Order(123, 21.f));
+                //add(new Item(234, 11.2f, "Xerox")); // compilation error: incompatible types
+            }
+        };
+
+        for(Order order : orders) {
+            System.out.println("order: " + order);
+        }
+    }
+}
