4b626b316000c3dd56c8698abc85d4a941e4f541
vim.cmd <vim.cmd@gmail.com>
2016-04-26 12:22:02 +0500

    added example of prioritizing threads (and using groups in comments)

---
+++ src/p02_classesAndLibrariesUsage/ch11_threads/sub03_threadsPriority/PriorityThread.java
@@ -0,0 +1,25 @@
+package p02_classesAndLibrariesUsage.ch11_threads.sub03_threadsPriority;
+
+/* # 3 # threads priority demonstration */
+
+public class PriorityThread extends Thread {
+    public PriorityThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 71; i++) {
+            if (i >= 70) {
+                System.out.println(getName() + " " + i + " FINISHED");
+            } else {
+                System.out.println(getName() + " " + i);
+            }
+            try {
+                Thread.sleep(2); // try sleep(0), sleep(10)
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
