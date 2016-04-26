414419b7238bff5ec79188b9bcc2b9898963b1cc
vim.cmd <vim.cmd@gmail.com>
2016-04-26 12:54:49 +0500

    added example of join and yield threads

---
+++ src/p02_classesAndLibrariesUsage/ch11_threads/sub04_threadManagement/YieldRunner.java
@@ -0,0 +1,24 @@
+package p02_classesAndLibrariesUsage.ch11_threads.sub04_threadManagement;
+
+/* # 5 # delay thread */
+
+public class YieldRunner {
+    public static void main(String[] args) {
+        new Thread() { // anonymous class
+            @Override
+            public void run() {
+                System.out.println("Starting thread 1");
+                Thread.yield(); // yield time for other threads (for ex.: if current thread priority is max, but it waits some data)
+                System.out.println("Finishing thread 1");
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Start thread 2");
+                System.out.println("Finish thread 2");
+            }
+        }.start();
+    }
+}
