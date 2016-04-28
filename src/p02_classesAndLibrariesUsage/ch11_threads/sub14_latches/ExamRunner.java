9211bdbd9471189f08276cf6a0cbfa48cf72ec44
vimcmd <vim.cmd@gmail.com>
2016-04-28 22:49:54 +0500

    added example of using latches on simple student exam example

---
+++ src/p02_classesAndLibrariesUsage/ch11_threads/sub14_latches/ExamRunner.java
@@ -0,0 +1,38 @@
+package p02_classesAndLibrariesUsage.ch11_threads.sub14_latches;
+
+/* # 29 # launch students group formation and executing verification of their tasks */
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ExamRunner {
+    public static void main(String[] args) {
+        final int NUMBER_OF_STUDENTS = 3;
+        ArrayList<Student> students = new ArrayList<>();
+
+        for(int i = 0; i < NUMBER_OF_STUDENTS; i++) {
+            final int NUMBER_OF_TASKS = 1 + ( new Random().nextInt(10) ); // at least one task for each student
+            Student student = new Student(new Random().nextInt(9_999), NUMBER_OF_TASKS);
+            System.out.println("Student #" + student.getIdStudent() + " have " + NUMBER_OF_TASKS + " tasks");
+
+            for(int j = 0; j < NUMBER_OF_TASKS; j++) {
+                Task task = new Task("Task#" + j);
+                student.addTask(task);
+            }
+
+            students.add(student);
+        }
+
+        students.forEach(Student::start);
+
+        Tutor tutor = new Tutor(students);
+
+        try {
+            Thread.sleep(new Random().nextInt(5_000)); // start verification thread with delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        tutor.start();
+    }
+}
---
+++ src/p02_classesAndLibrariesUsage/ch11_threads/sub14_latches/Tutor.java
@@ -0,0 +1,51 @@
+package p02_classesAndLibrariesUsage.ch11_threads.sub14_latches;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/* # 27 # tutor-thread, verifies students tasks */
+
+public class Tutor extends Thread {
+    private Integer idTutor;
+    private List<Student> students;
+
+    public Tutor() {
+        this.students = new ArrayList<>();
+    }
+
+    public Tutor(List<Student> students) {
+        this.students = students;
+    }
+
+    public Integer getIdTutor() {
+        return idTutor;
+    }
+
+    public void setIdTutor(Integer idTutor) {
+        this.idTutor = idTutor;
+    }
+
+    @Override
+    public void run() {
+        for(Student student : students) {
+            // check if student have tasks
+            List<Task> tasks = student.getTasks();
+            for(Task task : tasks) {
+
+                try {
+                    Thread.sleep(new Random().nextInt(1_000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // check if answer exists
+                int mark = 3 + new Random().nextInt(7); // set mark from 3 to 10
+                task.setMark(mark);
+                System.out.println("Mark " + mark + " for student #" + student.getIdStudent() + " for " + task.getContent());
+                student.getCountDownLatch().countDown();
+            }
+            System.out.println("All estimates are made for student #" + student.getIdStudent() + " complete");
+        }
+    }
+}
