6bebd77f93d00851b35bd900aa5983ca304faee1
vim.cmd <vim.cmd@gmail.com>
2016-04-29 11:00:47 +0500

    added example of usage Lock interface (flexible analogue of synchronized block)

---
+++ src/p02_classesAndLibrariesUsage/ch11_threads/sub16_synchronizedAlternative/LockingRunner.java
@@ -0,0 +1,20 @@
+package p02_classesAndLibrariesUsage.ch11_threads.sub16_synchronizedAlternative;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/* # 37 # run process of accessing threads to the resource */
+
+public class LockingRunner {
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 0; i < 5; i++) {
+            ResourcePairedStructure resource = new ResourcePairedStructure();
+            new ResourceThread("a" + new Random().nextInt(10), resource).start();
+            new ResourceThread("Z" + new Random().nextInt(10), resource).start();
+            new ResourceThread("#" + new Random().nextInt(10), resource).start();
+            TimeUnit.MILLISECONDS.sleep(1_000);
+            System.out.println("Result: " + resource);
+            System.out.println();
+        }
+    }
+}
