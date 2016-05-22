1e2818aa8c07ade36148fcd94f23c1344558cc46
vimcmd <vim.cmd@gmail.com>
2016-05-22 21:28:39 +0500

    tests template added

--- src/p02_classesAndLibrariesUsage/ch12_jdbc/sub05_chapterTasks/students/Runner.java
+++ src/p02_classesAndLibrariesUsage/ch12_jdbc/sub05_chapterTasks/students/Runner.java
@@ -1,36 +1,50 @@
 package p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students;
 
+import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.dao.PersistException;
+import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.domain.Group;
 import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.h2.H2DaoFactory;
+import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.h2.H2GroupDao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Runner {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, PersistException {
+
         H2DaoFactory h2DaoFactory = new H2DaoFactory();
-        Connection connection = h2DaoFactory.getConnection();
 
-        if (connection != null) {
-            DbHelper dbHelper = new DbHelper(connection);
-            dbHelper.recreateTables();
-            System.out.println("Done");
+        recreateTables(h2DaoFactory.getContext());
+
+        try {
+            h2DaoFactory.getContext().setAutoCommit(false);
+            // abstract jdbc dao
+            H2GroupDao groupDao = (H2GroupDao) h2DaoFactory.getDao(h2DaoFactory.getContext(), Group.class);
+
+            System.out.println(groupDao.getAll());
+
+            Group gr = groupDao.create();
+            gr.setDepartment("brand new department for hipsters");
+            gr.setNumber(102834);
+            groupDao.update(gr);
+            System.out.println("Gr id=" + gr.getId());
+            groupDao.getByPk(gr.getId());
+
+            System.out.println(groupDao.getAll());
+            h2DaoFactory.getContext().rollback();
+            h2DaoFactory.getContext().close();
+
+        } catch (PersistException e) {
+            e.printStackTrace();
         }
-
-
     }
 
-    //private static Connection getConnection() {
-    //    Connection connection = null;
-    //    try {
-    //        ResourceBundle resource = ResourceBundle.getBundle("properties.database_students");
-    //        String url = resource.getString("db.url");
-    //        String user = resource.getString("db.user");
-    //        String pass = resource.getString("db.password");
-    //        connection = DriverManager.getConnection(url, user, pass);
-    //    } catch (MissingResourceException | SQLException e) {
-    //        e.printStackTrace();
-    //    }
-    //
-    //    return connection;
-    //}
+    private static void recreateTables(Connection connection) {
+        DbHelper dbHelper = new DbHelper(connection);
+        try {
+            dbHelper.recreateTables();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ tests/p02_classesAndLibrariesUsage/ch12_jdbc/sub05_chapterTasks/students/dao/H2DaoTest.java
@@ -0,0 +1,55 @@
+package p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.domain.Group;
+import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.domain.Student;
+import p02_classesAndLibrariesUsage.ch12_jdbc.sub05_chapterTasks.students.h2.H2DaoFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class H2DaoTest extends GenericDaoTest<Connection> {
+
+    private static final DaoFactory<Connection> factory = new H2DaoFactory();
+    private Connection connection;
+    private GenericDao dao;
+
+    public H2DaoTest(Class clazz, Identified<Integer> notPersistedDto) {
+        super(clazz, notPersistedDto);
+    }
+
+    @Parameterized.Parameters
+    public static Collection getParameters() {
+        return Arrays.asList(new Object[][]{
+                {Group.class, Student.class}
+        });
+    }
+
+    @Before
+    public void setUp() throws PersistException, SQLException {
+        connection = factory.getContext();
+        connection.setAutoCommit(false);
+        dao = factory.getDao(connection, daoClass);
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        context().rollback();
+        context().close();
+    }
+
+    @Override
+    public GenericDao dao() {
+        return dao;
+    }
+
+    @Override
+    public Connection context() {
+        return connection;
+    }
+
+}
