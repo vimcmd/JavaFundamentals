89b1299bba88230cb46c61c742ffec7cea7ad4c7
vim.cmd <vim.cmd@gmail.com>
2016-05-25 13:23:29 +0500

    HttpServletRequest interface demo

--- src/p03_webApplicationTechnologies/ch15_servlets/sub01_basics/FirstServlet.java
+++ src/p03_webApplicationTechnologies/ch15_servlets/sub01_basics/FirstServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /* # 1 # simple servlet */
 
@@ -24,7 +26,30 @@ public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().print("This is " + this.getClass().getName() + ", using the GET method.");
+        PrintWriter out = resp.getWriter();
+
+        out.print("<div style='white-space: pre;'>"); // just separate lines in html
+
+        out.println("--- REQUEST ---");
+        out.println("This is " + this.getClass().getName() + ", using the GET method.");
+        out.println("method: " + req.getMethod());
+        out.println("request url: " + req.getRequestURL());
+        out.println("request uri: " + req.getRequestURI());
+        out.println("protocol: " + req.getProtocol());
+        out.println("remote address: " + req.getRemoteAddr());
+        out.println("remote user: " + req.getRemoteUser());
+        out.println("context path: " + req.getContextPath());
+        out.println("scheme: " + req.getScheme());
+
+        out.println("--- HEADERS ---");
+        Enumeration<String> headers = req.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String name = headers.nextElement();
+            String value = req.getHeader(name);
+            out.println(name + ": " + value);
+        }
+        out.println("</div>");
+
     }
 
     @Override
