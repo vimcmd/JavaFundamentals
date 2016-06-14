108c3d796f53d2e6a5f88fdbfe488e26863b308a
vim.cmd <vim.cmd@gmail.com>
2016-06-14 13:44:40 +0500

    added description in javadoc

--- src/main/java/com/github/vimcmd/javaFundamentals/p02_classesAndLibrariesUsage/ch07_string/sub03_regularExpressions/Abbreviator.java
+++ src/main/java/com/github/vimcmd/javaFundamentals/p02_classesAndLibrariesUsage/ch07_string/sub03_regularExpressions/Abbreviator.java
@@ -3,6 +3,9 @@ package com.github.vimcmd.javaFundamentals.p02_classesAndLibrariesUsage.ch07_str
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Abbreviates long words to form like i18n (internationalization).
+ */
 public class Abbreviator {
     public String abbreviate(String string) {
         Pattern pattern = Pattern.compile("(\\w)(\\w{2,})(\\w)"); // or use "(\b[A-Za-z])([A-Za-z]{2,})([A-Za-z]\b)"
