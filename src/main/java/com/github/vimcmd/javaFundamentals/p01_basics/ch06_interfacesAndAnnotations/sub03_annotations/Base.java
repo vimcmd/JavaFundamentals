11da50d65c8cd5fafa30396b822df3aab3799e34
vimcmd <vim.cmd@gmail.com>
2016-07-02 21:52:43 +0500

    added simple annotation example and its usage

---
+++ src/main/java/com/github/vimcmd/javaFundamentals/p01_basics/ch06_interfacesAndAnnotations/sub03_annotations/Base.java
@@ -0,0 +1,15 @@
+package com.github.vimcmd.javaFundamentals.p01_basics.ch06_interfacesAndAnnotations.sub03_annotations;
+
+// # 16 # class annotation primitive usage
+
+@BaseAction(level = 2, sqlRequest = "SELECT * FROM phonebook")
+public class Base {
+
+    public void doAction() {
+        Class<Base> f = Base.class;
+        BaseAction a = f.getAnnotation(BaseAction.class);
+        System.out.println(a.level());
+        System.out.println(a.sqlRequest());
+    }
+
+}
---
+++ src/main/java/com/github/vimcmd/javaFundamentals/p01_basics/ch06_interfacesAndAnnotations/sub03_annotations/BaseAction.java
@@ -0,0 +1,15 @@
+package com.github.vimcmd.javaFundamentals.p01_basics.ch06_interfacesAndAnnotations.sub03_annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// # 15 # simple class annotation
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface BaseAction {
+    int level();
+    String sqlRequest();
+}
